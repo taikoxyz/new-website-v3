5619f312775301ff4109e4afcc815cd04547d0f3
Karim <162329697+kimo-ice@users.noreply.github.com>
2026-04-03 21:55:09 +0200

    chore: update Discord invite link (#6)

--- src/content/data/navigation.ts
+++ src/content/data/navigation.ts
@@ -235,7 +235,7 @@ export const footerSocials: SocialLink[] = [
   {
     icon: "gsocial:discord",
     name: "discord",
-    url: "https://discord.com/invite/taikoxyz",
+    url: "https://discord.com/invite/taiko-984015101017346058",
   },
   {
     icon: "gsocial:twitter",
@@ -270,7 +270,7 @@ export const headerSocials: SocialLink[] = [
   { icon: "socials:twitter", url: "https://twitter.com/taikoxyz" },
   {
     icon: "socials:discord",
-    url: "https://discord.com/invite/taikoxyz",
+    url: "https://discord.com/invite/taiko-984015101017346058",
   },
 ];
 
